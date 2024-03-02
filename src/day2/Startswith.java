ee7bc8e894f277c5de3c72a8d66067d063c2d4db
Ashokselvan-98 <ashokspark1998a@gmail.com>
2024-03-02 12:14:47 +0530

    updated startswith

--- src/day2/Startswith.java
+++ src/day2/Startswith.java
@@ -2,7 +2,7 @@ package day2;
 
 public class Startswith {public static void main(String[]args) {
 	String a="java is a programe";
-	boolean b= a.startsWith("java is a");
+	boolean b= a.startsWith("hello");
 System.out.println(b);
 
 }
