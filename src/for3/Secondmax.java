a0fedaae73a50b149f265d3c02eacffce9e7ad96
Ashokselvan-98 <161914313+Ashokselvan-98@users.noreply.github.com>
2024-03-23 11:25:04 +0530

    Update Secondmax.java

--- src/for3/Secondmax.java
+++ src/for3/Secondmax.java
@@ -2,19 +2,21 @@ package for3;
 
 public class Secondmax {
 	public static void main(String[] args) {
-		int[] a = { 24, 75, 82, 64, 35, 90 };
+		int[] a = { 5, 90, 58, 20, 15 };
+
 		int max = a[0];
-		int secondmax = max;
+		int secondMax = 0;
+
 		for (int i = 0; i < a.length; i++) {
 			if (a[i] > max) {
-				secondmax = max;
+				secondMax = max;
 				max = a[i];
-				
-
+			} else if (a[i] > secondMax && a[i] != max) {
+				secondMax = a[i];
 			}
-
 		}
-		System.out.println(secondmax);
+		System.out.println(secondMax);
+
 	}
 
 }
